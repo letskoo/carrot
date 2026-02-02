a9c8ded34500da46523f5965818f0320af9b36d8
letskoo <kiwankoo@naver.com>
2026-02-03 03:54:04 +0900

    신청자 수 불러오는 중 - 로딩 중 애니메이션 추가

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -61,6 +61,8 @@ export default function HeroContent() {
     ? `최근 한달간 ${stats.last30DaysCount}명 신청 중 ( 누적 ${stats.totalCount}명 )`
     : "전국 가맹점이 신청 중입니다";
 
+  const statsClassName = loading ? "blink-animation" : "";
+
   return (
     <div className="pt-2 pb-5">
       <div>
@@ -74,7 +76,7 @@ export default function HeroContent() {
         <svg className="w-4 h-4 md:w-4 md:h-4" fill="currentColor" viewBox="0 0 24 24">
           <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/>
         </svg>
-        <span className="font-semibold">{statsText}</span>
+        <span className={`font-semibold ${statsClassName}`}>{statsText}</span>
       </div>
       </div>
     </div>
